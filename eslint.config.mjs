061edce8b6d8463cc6146da1532ded2823a0bd8b
Abel Yoshuara <abelyoshuara@gmail.com>
2024-05-28 22:09:29 +0800

    update eslint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -4,7 +4,10 @@ import tseslint from "typescript-eslint";
 import eslintConfigPrettier from "eslint-config-prettier";
 
 export default [
-  { languageOptions: { globals: globals.node } },
+  {
+    languageOptions: { globals: globals.node },
+    ignores: ["node_modules", ".expo"],
+  },
   eslintConfigPrettier,
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
